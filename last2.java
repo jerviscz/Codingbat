f984815c853e146a5fd1a2b1b019c210ae0458d5
Jason Chen <jerviscz@gmail.com>
2018-10-08 22:34:35 -0400

    Warmup-2 > last2

---
+++ last2.java
@@ -0,0 +1,19 @@
+/* Given a string, return the count of the number of times that a substring 
+ * length 2 appears in the string and also as the last 2 chars of the string, 
+ * so "hixxxhi" yields 1 (we won't count the end substring).
+ */
+ 
+public int last2(String str) {
+  if (str.length() < 2){
+    return 0;
+  }
+  String lastTwo = str.substring(str.length()-2);
+  int count = 0;
+  for (int i=0; i<str.length()-2; i++){
+    String check = str.substring(i, i+2);
+    if (check.equals(lastTwo)) {
+      count++;
+    }
+  }
+  return count;
+}
