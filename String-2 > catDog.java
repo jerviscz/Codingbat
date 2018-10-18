d4602fc09755c0841e2ffdbe4ac138c7637c14e3
Jason Chen <jerviscz@gmail.com>
2018-10-18 18:33:44 -0400

    String-2 > catDog

---
+++ String-2 > catDog.java
@@ -0,0 +1,17 @@
+/* Return true if the string "cat" and "dog" appear the same number of times 
+ * in the given string.
+ */
+
+ public boolean catDog(String str) {
+  int dogCount = 0;
+  int catCount = 0;
+  
+  for (int i = 0; i<str.length() -2; i++){
+    if (str.substring(i,i+3).equals("cat"))
+      catCount ++;
+    if (str.substring(i,i+3).equals("dog"))
+      dogCount ++;
+  }
+  return catCount == dogCount;
+  
+}
